1704315d85be3c4cae842abd3baaf1eb5528d050
Spencer Carter <scmountain@gmail.com>
2017-01-23 01:54:45 -0700

    final submission

---
+++ app/models/task.rb
@@ -0,0 +1,36 @@
+require 'sqlite3'
+class Task
+
+  attr_reader :title, :description, :id
+
+  def initialize
+    @id = task_params["id"] if task_params["id"]
+  end
+
+  def save
+    @database.execute("INSERT INTO tasks (title, description) VALUES (?, ?);", @title, @description)
+  end
+
+  def self.all
+    database = SQLite3::Database.new('db/task_manager_development.db')
+    database.results_as_hash = true
+    tasks = database.execute("SELECT * FROM tasks")
+    tasks.map do |task|
+      Task.new(task)
+    end
+  end
+
+  def self.find(id)
+    database = SQLite3::Database.new('db/task_manager_development.db')
+    database.results_as_hash = true
+    task = database.execute("SELECT * FROM tasks WHERE id = ?", id).first
+    Task.new(task)
+  end
+
+  def self.database
+    database = SQLite3::Database.new('db/task_manager_development.db')
+    database.results_as_hash = true
+    database
+  end
+
+end
